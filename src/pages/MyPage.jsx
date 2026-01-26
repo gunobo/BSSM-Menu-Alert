d1295830fc4f77e1c3b5a88ccfdb8f8923fcabca
gunobo <startea0716@gmail.com>
2026-01-26 18:27:42 +0900

    commit and push 260126-03

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,12 +1,34 @@
 import "../styles/mypage.css";
 import { getUser, logout } from "../utils/auth";
 import { useNavigate } from "react-router-dom";
+import bssmLogo from "../assets/bssmlogo.png";
 
 export default function MyPage() {
   const user = getUser();
   const navigate = useNavigate();
 
   return (
+    <>
+    <nav className="navbar">
+            <div className="nav-logo">
+              <img
+                src={bssmLogo}
+                alt="BSSM 로고"
+                onClick={() => window.location.href = "https://school.busanedu.net/bssm-h"}
+              />
+              <h2 onClick={() => navigate("/")}>BSSM 급식 알리미</h2>
+            </div>
+            <div className="nav-buttons">
+                <>
+                  <button
+                    onClick={() => navigate("/mypage")}
+                    className="nav-btn"
+                  >
+                    마이페이지
+                  </button>
+                </>
+            </div>
+          </nav>
     <div className="mypage-wrapper">
       <div className="mypage-card">
         <h2>마이페이지</h2>
@@ -20,6 +42,16 @@ export default function MyPage() {
           <div className="info-item">
             <span>권한</span>
             <div>{user.role}</div>
+          </div>
+
+          <div className="info-item">
+            <span>좋아하는 메뉴</span>
+            <div>{user.menu}</div>
+          </div>
+
+          <div className="info-item">
+            <span>알레르기</span>
+            <div>{user.menu}</div>
           </div>
         </div>
 
@@ -43,5 +75,6 @@ export default function MyPage() {
         </div>
       </div>
     </div>
+    </>
   );
 }
