8231cd574ba836327293668ad4d30a2e8136dacb
임제민 <startea0716@naver.com>
2026-07-25 19:53:30 +0900

    123

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ import TeacherTimetablePage from "./pages/TeacherTimetable";
 import ReportList from "./pages/ReportList";
 import ReportDetail from "./pages/ReportDetail";
 import SchedulePage from "./pages/Schedule";
-import SchoolNotice from "./pages/SchoolNotice";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -150,7 +149,6 @@ export default function App() {
       <Route path="/timetable" element={<TimetablePage />} />
       <Route path="/timetable/t" element={<TeacherTimetablePage />} />
       <Route path="/schedule" element={<SchedulePage />} />
-      <Route path="/school-notice" element={<SchoolNotice />} />
       <Route path="/my-report" element={<ReportList />} />
       <Route path="/my-report/:id" element={<ReportDetail />} />
       <Route
